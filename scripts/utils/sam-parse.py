9d41689d92fc4d538cb10eee9b4167d695e3195d
Yee Fay Lim <yeefay.lim@gmail.com>
2018-10-18 12:16:52 -0700

    Removed unnecessary missing sequence detection code from BWA SAM output parser

--- scripts/utils/sam-parse.py
+++ scripts/utils/sam-parse.py
@@ -5,7 +5,6 @@ import sys
 SAMFILE = sys.argv[1]
 OUTFILE = sys.argv[2]
 ERRORFILE = sys.argv[3]
-MISSINGFILE = sys.argv[4]
 ID_COL = 0
 FLAGS_COL = 1
 MAPQ_COL = 4
@@ -61,7 +60,6 @@ csv.field_size_limit(sys.maxsize)
 with open(SAMFILE, newline='') as samfile:
     reader = csv.reader(samfile, delimiter='\t')
     error_seqs = []
-    missing_seqs = []
     with open(OUTFILE, 'w', newline='') as outfile:
         writer = csv.DictWriter(outfile, delimiter='\t', fieldnames=['ID', 'Mapped', 'Matches', 'Others', 'Other CIGARs', 'MAPQ (unique match only)', 'GC content'])
         writer.writeheader()
@@ -79,24 +77,16 @@ with open(SAMFILE, newline='') as samfile:
                     elif error_seqs[-1] != seq_dict['ID']: # if listed as unmapped and then mapped...
                         error_seqs.append(seq_dict['ID'])
             else:
-                if int(row[ID_COL]) == seq_dict['ID'] + 1:
-                    finalise(seq_dict, other_cigars)
-                    writer.writerow(seq_dict)
-                    seq_dict = init_seq_dict(row)
-                    other_cigars = []
-                    if seq_dict['Mapped']:
-                        update_match_info(row, seq_dict, other_cigars)
-                else:
-                    missing_seqs.extend(range(seq_dict['ID'] + 1, int(row[0])))
-            i += 1
+                finalise(seq_dict, other_cigars)
+                writer.writerow(seq_dict)
+                seq_dict = init_seq_dict(row)
+                other_cigars = []
+                if seq_dict['Mapped']:
+                    update_match_info(row, seq_dict, other_cigars)
         finalise(seq_dict, other_cigars)
         writer.writerow(seq_dict)
     with open(ERRORFILE, 'w', newline='') as errorfile:
         writer = csv.writer(errorfile)
         for id in error_seqs:
-            writer.writerow([id])
-    with open(MISSINGFILE, 'w', newline='') as missingfile:
-        writer = csv.writer(missingfile)
-        for id in missing_seqs:
             writer.writerow([id])
 
