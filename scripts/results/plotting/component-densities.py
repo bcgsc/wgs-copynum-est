b5ed36292fb4c8894c4606a83971ca5b6f55c933
Yee Fay Lim <yeefay.lim@gmail.com>
2019-03-18 12:25:20 -0700

    1. Refactor (alignment and estimated) densities plotting code to work with variable number of copy-number components. 2. General code refactor.

--- scripts/results/plotting/component-densities.py
+++ scripts/results/plotting/component-densities.py
@@ -2,125 +2,85 @@ import matplotlib
 matplotlib.use('agg')
 
 import matplotlib.pyplot as plt
+import math as m
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import sys
 
+
 SEQ_EST_ALN_FILE = sys.argv[1]
 PLOTS_FILE_PREFIX = sys.argv[2]
-NUM_CLASSES = 6
 
-def fit_est_kdes(seqs, kdes, counts):
-    vals = seqs[seqs.est1 == 1].mean_depth.values
-    if len(vals) > 0:
-        kdes[1] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[1].fit(bw='scott')
-        counts[1] = len(vals)
-    vals = seqs[seqs.est1 == 2].mean_depth.values
-    if len(vals) > 0:
-        kdes[2] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[2].fit(bw='scott')
-        counts[2] = len(vals)
-    vals = seqs[seqs.est1 == 3].mean_depth.values
-    if len(vals) > 0:
-        kdes[3] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[3].fit(bw='scott')
-        counts[3] = len(vals)
-    vals = seqs[seqs.est1 == 4].mean_depth.values
-    if len(vals) > 0:
-        kdes[4] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[4].fit(bw='scott')
-        counts[4] = len(vals)
-    vals = seqs[seqs.est1 >= 5].mean_depth.values
-    if len(vals) > 0:
-        kdes[5] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[5].fit(bw='scott')
-        counts[5] = len(vals)
+def kde_and_count_copynum(vals, c, kdes, counts):
+  if len(vals) > 0:
+    kdes[c] = sm.nonparametric.KDEUnivariate(vals)
+    kdes[c].fit(bw='scott')
+    counts[c] = len(vals)
 
-def fit_aln_kdes(seqs, kdes, counts):
-    vals = seqs[seqs.alns == 0].mean_depth.values
-    if len(vals) > 0:
-        kdes[0] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[0].fit(bw='scott')
-        counts[0] = len(vals)
-    vals = seqs[(seqs.alns == 1) | (seqs.alns == 2)].mean_depth.values
-    if len(vals) > 0:
-        kdes[1] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[1].fit(bw='scott')
-        counts[1] = len(vals)
-    vals = seqs[(seqs.alns == 3) | (seqs.alns == 4)].mean_depth.values
-    if len(vals) > 0:
-        kdes[2] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[2].fit(bw='scott')
-        counts[2] = len(vals)
-    vals = seqs[(seqs.alns == 5) | (seqs.alns == 6)].mean_depth.values
-    if len(vals) > 0:
-        kdes[3] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[3].fit(bw='scott')
-        counts[3] = len(vals)
-    vals = seqs[(seqs.alns == 7) | (seqs.alns == 8)].mean_depth.values
-    if len(vals) > 0:
-        kdes[4] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[4].fit(bw='scott')
-        counts[4] = len(vals)
-    vals = seqs[seqs.alns >= 9].mean_depth.values
-    if len(vals) > 0:
-        kdes[5] = sm.nonparametric.KDEUnivariate(vals)
-        kdes[5].fit(bw='scott')
-        counts[5] = len(vals)
+def fit_est_kdes(seqs, cpnums, kdes, counts):
+  for c in cpnums[:-1]:
+    kde_and_count_copynum(seqs.loc[seqs.likeliest_copynum == c,].mean_kmer_depth.values, c, kdes, counts)
+  kde_and_count_copynum(seqs.loc[seqs.likeliest_copynum >= cpnums[-1],].mean_kmer_depth.values, cpnums[-1], kdes, counts)
 
-def plot_kdes(ax, kde_grid, est_kdes, aln_kdes, kde_all, est_counts, aln_counts, all_count):
-    ax.plot(kde_grid, est_kdes[1].evaluate(kde_grid) * est_counts[1] / all_count, 'r--', lw=1, label = "Estimated copy # 1")
-    ax.plot(kde_grid, est_kdes[2].evaluate(kde_grid) * est_counts[2] / all_count, 'g--', lw=1, label = "Estimated copy # 2")
-    ax.plot(kde_grid, est_kdes[3].evaluate(kde_grid) * est_counts[3] / all_count, 'c--', lw=1, label = "Estimated copy # 3")
-    ax.plot(kde_grid, est_kdes[4].evaluate(kde_grid) * est_counts[4] / all_count, 'm--', lw=1, label = "Estimated copy # 4")
-    ax.plot(kde_grid, est_kdes[5].evaluate(kde_grid) * est_counts[5] / all_count, 'y--', lw=1, label = "Estimated copy # 5+")
-    ax.plot(kde_grid, aln_kdes[0].evaluate(kde_grid) * aln_counts[0] / all_count, 'b', lw=1, label = "True copy # 0")
-    ax.plot(kde_grid, aln_kdes[1].evaluate(kde_grid) * aln_counts[1] / all_count, 'r', lw=1, label = "True copy # 1")
-    ax.plot(kde_grid, aln_kdes[2].evaluate(kde_grid) * aln_counts[2] / all_count, 'g', lw=1, label = "True copy # 2")
-    ax.plot(kde_grid, aln_kdes[3].evaluate(kde_grid) * aln_counts[3] / all_count, 'c', lw=1, label = "True copy # 3")
-    ax.plot(kde_grid, aln_kdes[4].evaluate(kde_grid) * aln_counts[4] / all_count, 'm', lw=1, label = "True copy # 4")
-    ax.plot(kde_grid, aln_kdes[5].evaluate(kde_grid) * aln_counts[5] / all_count, 'y', lw=1, label = "True copy # 5+")
-    ax.plot(kde_grid, kde_all.evaluate(kde_grid), 'k', lw=1, label = "All sequences")
+def fit_aln_kdes(seqs, cpnums, kdes, counts):
+  for c in cpnums[:-1]:
+    kde_and_count_copynum(seqs.loc[seqs.alns == c,].mean_kmer_depth.values, c, kdes, counts)
+  kde_and_count_copynum(seqs.loc[seqs.alns >= cpnums[-1],].mean_kmer_depth.values, cpnums[-1], kdes, counts)
 
-est_kdes = [None] * NUM_CLASSES
-est_counts = [None] * NUM_CLASSES
-aln_kdes = [None] * NUM_CLASSES
-aln_counts = [None] * NUM_CLASSES
-kde_grid = np.linspace(0, 200, 200 * 100 + 1)
+def plot_kdes(cpnums, ax, kde_grid, est_kdes, aln_kdes, kde_all, est_counts, aln_counts, all_count):
+  for c in cpnums:
+    idx = int(c + 1)
+    if c < 1:
+      idx = m.ceil(c)
+    if est_kdes[c] is not None:
+      ax.plot(kde_grid, est_kdes[c].evaluate(kde_grid) * est_counts[c] / all_count, color = COLOURS[idx], linestyle = '--', lw = 1, label = 'Estimated copy # ' + str(c))
+    if aln_kdes[c] is not None:
+      ax.plot(kde_grid, aln_kdes[c].evaluate(kde_grid) * aln_counts[c] / all_count, color = COLOURS[idx], lw = 1, label = 'True copy # ' + str(c))
+  ax.plot(kde_grid, kde_all.evaluate(kde_grid), 'k', lw = 1, label = "All sequences")
+
+def compute_and_plot_kdes(seqs, title_suffix, filename_prefix):
+  est_max = min(MAX_INT_COPYNUM, int(seqs.likeliest_copynum.max()))
+  cpnums = range(est_max + 1)
+  if HALF:
+    cpnums = [0, 0.5] + list(range(1, est_max + 1))
+
+  est_kdes = pd.Series([None] * len(cpnums), index = cpnums)
+  est_counts = pd.Series([0] * len(cpnums), index = cpnums)
+  aln_kdes = pd.Series([None] * len(cpnums), index = cpnums)
+  aln_counts = pd.Series([0] * len(cpnums), index = cpnums)
+  fit_est_kdes(seqs, cpnums, est_kdes, est_counts)
+  fit_aln_kdes(seqs, cpnums, aln_kdes, aln_counts)
+
+  depths = np.copy(seqs.mean_kmer_depth.values)
+  depths.sort()
+  kde_all = sm.nonparametric.KDEUnivariate(depths)
+  kde_all.fit(bw='scott')
+
+  grid_min = (1 - MIN_OFFSET_FRACTION) * depths[0]
+  grid_max = min(seqs.loc[seqs.likeliest_copynum == est_max,].mean_kmer_depth.mean() * 2, depths[-1]) + MAX_OFFSET
+  kde_grid = np.linspace(grid_min, grid_max, 100 * (grid_max - grid_min) + 1) # can replace 100 by some other density
+  fig, ax = plt.subplots()
+  plot_kdes(cpnums, ax, kde_grid, est_kdes, aln_kdes, kde_all, est_counts, aln_counts, len(depths))
+  plt.legend()
+  ax.set_title('Densities for estimated and true copy numbers, sequences ' + title_suffix)
+  fig.savefig(filename_prefix + '.png')
+
 
 all_seqs = pd.read_csv(SEQ_EST_ALN_FILE)
 all_seqs.rename(index=str, inplace=True,
-            columns={ 'Length': 'length', 'Average depth': 'mean_depth', 'GC %': 'GC',
-                      'Est. 1st label': 'est1', 'Est. 2nd label': 'est2', 'Est. 3rd label': 'est3',
-                      'Ref. alns': 'alns', 'Ref. other alns': 'other_alns', 'Other alns CIGARs': 'other_aln_cigars', 'MAPQ (unique aln only)': 'unique_aln_mapq' })
-all_seqs = all_seqs[all_seqs.mean_depth <= 800]
+    columns = { 'Length': 'length', 'Average depth': 'mean_kmer_depth', 'GC %': 'GC', 'Likeliest copy #': 'likeliest_copynum',
+                'Alignments (alns)': 'alns', 'Other alns': 'other_alns', 'Other-aln CIGARs': 'other_aln_cigars', 'MAPQ (unique aln only)': 'unique_aln_mapq' })
 
-fit_est_kdes(all_seqs, est_kdes, est_counts)
-fit_aln_kdes(all_seqs, aln_kdes, aln_counts)
-vals = all_seqs.mean_depth.values
-kde_all = sm.nonparametric.KDEUnivariate(vals)
-kde_all.fit(bw='scott')
-all_count = len(vals)
-fig, ax = plt.subplots()
-plot_kdes(ax, kde_grid, est_kdes, aln_kdes, kde_all, est_counts, aln_counts, all_count)
-plt.legend()
-ax.set_title('Densities for estimated and true copy numbers, sequences of all lengths')
-fig.savefig(PLOTS_FILE_PREFIX + '.png')
+MAX_OFFSET, MIN_OFFSET_FRACTION = 1, 0.1
+HALF = ((all_seqs.likeliest_copynum == 0.5).sum() > 0)
+MAX_INT_COPYNUM = 8 + int(not(HALF))
+COLOURS = [ 'xkcd:azure', 'xkcd:coral', 'xkcd:darkgreen', 'xkcd:gold', 'xkcd:plum', 'xkcd:darkblue', 'xkcd:olive', 'xkcd:magenta', 'xkcd:chocolate', 'xkcd:yellowgreen' ]
 
+compute_and_plot_kdes(all_seqs, 'of all lengths', PLOTS_FILE_PREFIX)
 lb = 0
 for ub in [100, 1000, 10000, np.Inf]:
-    seqs = all_seqs[(all_seqs.length >= lb) & (all_seqs.length < ub)]
-    fit_est_kdes(seqs, est_kdes, est_counts)
-    fit_aln_kdes(seqs, aln_kdes, aln_counts)
-    vals = seqs.mean_depth.values
-    kde_all = sm.nonparametric.KDEUnivariate(vals)
-    kde_all.fit(bw='scott')
-    all_count = len(vals)
-    fig, ax = plt.subplots()
-    plot_kdes(ax, kde_grid, est_kdes, aln_kdes, kde_all, est_counts, aln_counts, all_count)
-    plt.legend()
-    ax.set_title('Densities for estimated and true copy numbers, sequences with length in [' + str(lb) + ', ' + str(ub) + ')', fontsize=10)
-    fig.savefig(PLOTS_FILE_PREFIX + '_len-gte' + str(lb) + 'lt' + str(ub) + '.png')
-    lb = ub
+  seqs = all_seqs.loc[(all_seqs.length >= lb) & (all_seqs.length < ub),]
+  if seqs.shape[0] > 0:
+    compute_and_plot_kdes(seqs, 'with length in [' + str(lb) + ', ' + str(ub) + ')', PLOTS_FILE_PREFIX + '_len-gte' + str(lb) + 'lt' + str(ub))
+  lb = ub
