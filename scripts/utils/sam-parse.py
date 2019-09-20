f64e77aa4a8a1184f2100c983825951537a19ed8
Yee Fay Lim <yeefay.lim@gmail.com>
2019-09-20 13:30:59 -0700

    1. Fix incorrect [mis]mapped sequence counting. 2. Remove incorrect and unnecessary error logging.

--- scripts/utils/sam-parse.py
+++ scripts/utils/sam-parse.py
@@ -17,11 +17,11 @@ def compute_gc_content(seq):
     return (gc_count / len(seq))
 
 def init_seq_dict(row):
-    return {'ID': int(row[ID_COL]), 'Mapped': is_mapped(int(row[FLAGS_COL])), 'Matches': 0, 'Others': 0, 'MAPQ sum': 0, 'GC content': compute_gc_content(row[SEQ_COL]) }
+    return { 'ID': int(row[ID_COL]), 'Matches': 0, 'Clipped': 0, 'MAPQ sum': 0, 'GC content': compute_gc_content(row[SEQ_COL]) }
 
 def update_match_info(row, seq_dict):
-    if is_alnmt_hard_clipped(row[CIGAR_COL]):
-        seq_dict['Others'] += 1
+    if is_alnmt_clipped(row[CIGAR_COL]):
+        seq_dict['Clipped'] += 1
     else:
         seq_dict['Matches'] += 1
         seq_dict['MAPQ sum'] += int(row[MAPQ_COL])
@@ -43,46 +43,38 @@ def is_match(cigar):
         return True
     return False
 
-def is_alnmt_hard_clipped(cigar):
-    if re.search('H', cigar):
-        return True
-    return False
+def is_alnmt_clipped(cigar):
+    hsplit = cigar.split('H')
+    if len(hsplit) == 1:
+        return (len(cigar.split('S')) > 1)
+    return True
 
 
 argparser = argparse.ArgumentParser(description="Parse SAM file output by BWA-MEM alignment of sequences/unitigs in FASTA file to [mutated] reference")
 argparser.add_argument("samfilename", type=str, help="BWA-MEM output SAM file")
 argparser.add_argument("outfilename", type=str, help="File name for sequence/unitig reference alignment counts output")
-argparser.add_argument("errorfilename", type=str, help="Error log file name")
 args = argparser.parse_args()
 
 csv.field_size_limit(sys.maxsize)
 
 with open(args.samfilename, newline='') as samfile:
     reader = csv.reader(samfile, delimiter='\t')
-    error_seqs = []
     with open(args.outfilename, 'w', newline='') as outfile:
-        writer = csv.DictWriter(outfile, delimiter='\t', fieldnames=['ID', 'Mapped', 'Matches', 'Others', 'MAPQ sum', 'GC content'])
+        writer = csv.DictWriter(outfile, delimiter='\t', fieldnames=['ID', 'Matches', 'Clipped', 'MAPQ sum', 'GC content'])
         writer.writeheader()
         row = next(reader)
         seq_dict = init_seq_dict(row)
-        if seq_dict['Mapped']:
+        if is_mapped(int(row[FLAGS_COL])):
             update_match_info(row, seq_dict)
         i = 1
         for row in reader:
             if int(row[ID_COL]) == seq_dict['ID']:
                 if is_mapped(int(row[FLAGS_COL])):
-                    if seq_dict['Mapped']:
-                        update_match_info(row, seq_dict)
-                    elif error_seqs[-1] != seq_dict['ID']: # if listed as unmapped and then mapped...
-                        error_seqs.append(seq_dict['ID'])
+                    update_match_info(row, seq_dict)
             else:
                 writer.writerow(seq_dict)
                 seq_dict = init_seq_dict(row)
-                if seq_dict['Mapped']:
+                if is_mapped(int(row[FLAGS_COL])):
                     update_match_info(row, seq_dict)
         writer.writerow(seq_dict)
-    with open(args.errorfilename, 'w', newline='') as errorfile:
-        writer = csv.writer(errorfile)
-        for id in error_seqs:
-            writer.writerow([id])
 
