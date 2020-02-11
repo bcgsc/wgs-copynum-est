5f484adc3b02c255bc37df70622f714cb7742b81
Yee Fay Lim <yeefay.lim@gmail.com>
2020-02-11 11:13:04 -0800

    Add functions supporting likeliest copy number assignment to utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -51,6 +51,65 @@ def get_contig_length_gps(seqs, seqs_len):
   length_gps.reverse()
   return length_gps
 
+def compute_likeliest_copynum_indices(maxdensity_copynums):
+    maxdens_cpnums_change_idxs = np.where(np.diff(maxdensity_copynums))[0]
+    if maxdens_cpnums_change_idxs.size == 0:
+        maxdens_cpnums_change_idxs = np.array([-1])
+    maxdens_cpnums_change_idxs += 1
+    return maxdens_cpnums_change_idxs
+
+def compute_likeliest_copynums(maxdensity_copynums, maxdens_cpnums_change_idxs):
+    maxdens_change_cpnums = pd.Series([0.0] * (maxdens_cpnums_change_idxs.size + 1))
+    maxdens_change_cpnums[0] = maxdensity_copynums.iloc[0]
+    if maxdens_change_cpnums.size > 1:
+        maxdens_change_cpnums[1:] = maxdensity_copynums.iloc[maxdens_cpnums_change_idxs]
+    return maxdens_change_cpnums
+
+def assign_copynums_and_bounds(likeliest_copynums, likeliest_copynum_ubs, copynums):
+    copynum_assnmts, copynums_unique = [likeliest_copynums[0]], { likeliest_copynums[0] }
+    # Note: 0 does not have an entry in copynum_lbs and copynum_ubs.
+    copynum_lbs = pd.Series(np.inf, index=copynums)
+    copynum_ubs = pd.Series(np.inf, index=copynums)
+    copynum_lbs[copynum_assnmts[0]] = 0
+    # Initial assignments might be slightly out of order: have to infer orderly final assignments
+    # Assume that 1. Larger copy#s don't occur in order before smaller copy#s, e.g. 1, 3, 4, 2 does not occur
+    # (whereas 1, 4, 3, 2 could [copy#4 variance is larger than that of 3])
+    # 2a. Out-of-order copy#s are always followed eventually by in-order copy#s, e.g.: 1, 3, 2 will be followed eventually by a copy# >= 3
+    # b. With one possible exception at the end (accounted for by the last "if" clause)
+    reserve_copynum, reserve_bd = 0, np.inf
+    for i in range(1, len(likeliest_copynums)):
+        if likeliest_copynums[i] not in copynums_unique:
+            if likeliest_copynums[i] < copynum_assnmts[-1]:
+                reserve_copynum, reserve_bd = copynum_assnmts[-1], copynum_lbs[copynum_assnmts[-1]]
+                copynum_lbs[copynum_assnmts[-1]] = np.inf
+                copynums_unique.remove(copynum_assnmts[-1])
+                copynum_assnmts.pop()
+            if len(copynum_assnmts) == 0:
+                copynum_lbs[likeliest_copynums[i]] = 0
+            else:
+                copynum_lbs[likeliest_copynums[i]] = likeliest_copynum_ubs[i-1]
+                copynum_ubs[copynum_assnmts[-1]] = likeliest_copynum_ubs[i-1]
+            copynum_assnmts.append(likeliest_copynums[i])
+            copynums_unique.add(likeliest_copynums[i])
+    # To keep from putting upper bound of penultimate cpnum at intersection between higher copy number and tail of lower copy number.
+    # Ideally, would record and check whether reserve_copynum and copynum_assnmts[-1] were assigned in the same iteration
+    # (the current order seems likelier to be correct if multiple copy numbers were assigned after the reserve), but
+    # this already works as well as or better than omitting it (in like 29/30 cases), and changing it would require a whole bunch more testing
+    if len(copynum_assnmts) > 1 and (copynum_assnmts[-1] < reserve_copynum):
+        copynum_lbs[copynum_assnmts[-1]] = np.inf
+        copynum_assnmts.pop()
+        copynum_lbs[reserve_copynum] = reserve_bd
+        copynum_ubs[copynum_assnmts[-1]] = reserve_bd
+        copynum_assnmts.append(reserve_copynum)
+    return (copynum_assnmts, copynum_lbs, copynum_ubs)
+
+def get_cpnums_and_bounds(copynum_densities, copynums):
+  maxdensity_copynums = copynum_densities.idxmax()
+  likeliest_copynum_ub_idxs = compute_likeliest_copynum_indices(maxdensity_copynums)
+  likeliest_copynums = compute_likeliest_copynums(maxdensity_copynums, likeliest_copynum_ub_idxs)
+  likeliest_copynum_ubs = copynum_densities.columns[likeliest_copynum_ub_idxs]
+  return(assign_copynums_and_bounds(likeliest_copynums, likeliest_copynum_ubs, copynums))
+
 def wide_to_long_from_listcol(df, variable_colname):
   obs_counts = [len(x) for x in chain.from_iterable(df[variable_colname])]
   df_long = pd.DataFrame(index=range(sum(obs_counts)), columns=df.columns)
