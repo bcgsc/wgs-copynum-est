1020d187304ee25ab6a374c421fa5fd79d6e34ef
Yee Fay Lim <yeefay.lim@gmail.com>
2020-02-19 12:25:11 -0800

    Remove probably superfluous comment about Gaussian parameter constraints

--- scripts/est.py
+++ scripts/est.py
@@ -535,7 +535,7 @@ for longest_seqs_mode1_copynum in ([0.5] * int(not(haploid_or_trivial)) + [1.0])
     length_gp_sigmas = [None] * length_gps_count
     copynum_stats_hash = { col: [] for col in COPYNUM_STATS_COLS }
     aic_current = 0
-    mode, mode_min, mode_max, sigma_min = np.nan, 0, np.inf, 0 # 0 mins because NONNEG_CONSTANT resulted in no CIs (numerical instability?)
+    mode, mode_min, mode_max, sigma_min = np.nan, 0, np.inf, 0
 
     for len_gp_idx in range(length_gps_count - 1, -1, -1):
         print(len_gp_idx)
