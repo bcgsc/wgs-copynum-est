49b58645e77a6a274a68cb4e6cd55ddab347a4e2
Yee Fay Lim <yeefay.lim@gmail.com>
2021-03-21 23:46:11 -0700

    Renamed performance metrics computation script

--- scripts/utils/results/compute-stats-loop.sh
+++ scripts/utils/results/compute-stats-loop.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-python ${WGS_COPYNUM_EST_HOME}/scripts/results/general/compute-summary-stats.py aln-est_counts.csv counts summary-stats
+python ${WGS_COPYNUM_EST_HOME}/scripts/results/general/compute_summary_stats.py aln-est_counts.csv counts summary-stats
 for i in `ls aln-est_counts_gte*csv`; do
   e=$(echo $i | perl -ne 'chomp; if (/aln-est_counts_(gte\d+lt(e\d+|inf))\.csv/) { print "$1"; }')
-  python ${WGS_COPYNUM_EST_HOME}/scripts/results/general/compute-summary-stats.py aln-est_counts_${e}.csv counts_${e} summary-stats_${e}
+  python ${WGS_COPYNUM_EST_HOME}/scripts/results/general/compute_summary_stats.py aln-est_counts_${e}.csv counts_${e} summary-stats_${e}
 done
 for i in 100 1000 10000; do
   if [ -f aln-est_counts_lt${i}.csv ]; then
-    python ${WGS_COPYNUM_EST_HOME}/scripts/results/general/compute-summary-stats.py aln-est_counts_lt${i}.csv counts_lt${i} summary-stats_lt${i}
+    python ${WGS_COPYNUM_EST_HOME}/scripts/results/general/compute_summary_stats.py aln-est_counts_lt${i}.csv counts_lt${i} summary-stats_lt${i}
   fi
 done
 if [ -f aln-est_counts_gte10000.csv ]; then
-  python ${WGS_COPYNUM_EST_HOME}/scripts/results/general/compute-summary-stats.py aln-est_counts_gte10000.csv counts_gte10000 summary-stats_gte10000
+  python ${WGS_COPYNUM_EST_HOME}/scripts/results/general/compute_summary_stats.py aln-est_counts_gte10000.csv counts_gte10000 summary-stats_gte10000
 fi
 
