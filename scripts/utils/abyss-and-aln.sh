8fc03b5ec558845ec057693541c4010648655c2c
Yee Fay Lim <yeefay.lim@gmail.com>
2021-03-21 18:42:00 -0700

    Allow user-specified k parameter for BWA-MEM in assembly and alignment script

--- scripts/utils/abyss-and-aln.sh
+++ scripts/utils/abyss-and-aln.sh
@@ -2,17 +2,32 @@
 
 set -x
 
+samparse_folder="utils"
+
+while getopts "r" opt; do
+  case $opt in
+    r)
+      samparse_folder="real-genomes"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
 k=$1; shift
 abyss_t=$1; shift
 reads1=$1; shift
 reads2=$1; shift
 name=$1; shift
+bwa_k=$1; shift
 bwa_t=$1; shift
 refpath=$1; shift
 
 mkdir -p abyss-out
 cd abyss-out
-${WGS_COPYNUM_EST_HOME}/scripts/run-abyss.sh $k $abyss_t ../$reads1 ../$reads2 $name
+${WGS_COPYNUM_EST_HOME}/scripts/utils/run-abyss.sh $k $abyss_t ../$reads1 ../$reads2 $name
 
 cd ..
 mkdir -p aln
@@ -20,7 +35,7 @@ mkdir -p aln
 # Run BWA-mem to align unitigs to reference; output to SAM file.
 # Recommended value for -t: 1. Otherwise output is interleaved and complicates parsing.
 # Other args to BWA-MEM, e.g.: -w 5 -d 50 -c 10000
-bwa mem -a -k $k -t $bwa_t $@ $refpath abyss-out/${name}-2.fa > aln/${name}_aln.sam
+bwa mem -a -k $bwa_k -t $bwa_t $@ $refpath abyss-out/${name}-2.fa > aln/${name}_aln.sam
 
 # Parse SAM; write to <ref_aln_counts_csv> (substitute perfect reads SAM file for simulated dataset if appropriate)
 python ${WGS_COPYNUM_EST_HOME}/scripts/utils/sam_parse.py aln/${name}_aln.sam aln/${name}_aln_parsed.tsv
