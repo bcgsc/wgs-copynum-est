d6a889da1723354bc4ee2f8f7474666c3f807de5
Yee Fay Lim <yeefay.lim@gmail.com>
2021-03-21 20:14:50 -0700

    Updated BWA-MEM output parser reference in assembly and alignment script to reflect new name, I think—confused during Git rebase.

--- scripts/utils/abyss-and-aln.sh
+++ scripts/utils/abyss-and-aln.sh
@@ -23,6 +23,6 @@ mkdir -p aln
 bwa mem -a -k $k -t $bwa_t $@ $refpath abyss-out/${name}-2.fa > aln/${name}_aln.sam
 
 # Parse SAM; write to <ref_aln_counts_csv> (substitute perfect reads SAM file for simulated dataset if appropriate)
-python ${WGS_COPYNUM_EST_HOME}/scripts/utils/sam-parse.py aln/${name}_aln.sam aln/${name}_aln-counts.tsv
+python ${WGS_COPYNUM_EST_HOME}/scripts/utils/sam_parse.py aln/${name}_aln.sam aln/${name}_aln_parsed.tsv
 
 set +x
