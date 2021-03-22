d933d7c572ddc900fd11a51dba25465bd2b7b3eb
Yee Fay Lim <yeefay.lim@gmail.com>
2021-03-22 00:16:15 -0700

    1. Updated GenomeScope script. 2. Updated GenomeScope wrapper script to reflect (1) and remove ntCard invocation.

--- scripts/genomescope/run-genomescope.sh
+++ scripts/genomescope/run-genomescope.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-first=$1
-increment=$2
-last=$3
-prefix=$4
-reads1=$5
-reads2=$6
-readlen=$7
-
 set -x
 
-mkdir -p hist
+mkdir -p out
 cd hist
-ntcard -t2 -k21 -p $prefix ../$reads1 ../$reads2
-cd ..
-${WGS_COPYNUM_EST_HOME}/scripts/explore/run-and-plot_ntcard.sh $first $increment $last $prefix $reads1 $reads2
-
-mkdir out
-cd hist
-${WGS_COPYNUM_EST_HOME}/scripts/utils/ntcard-to-jellyfish.sh
+${WGS_COPYNUM_EST_HOME}/scripts/genomescope/ntcard-to-jellyfish.sh
 for f in `ls`; do
   k=$(echo $f | perl -ne 'chomp; if (/.*k(\d+)\.hist/) { print "$1"; }')
-  /gsc/software/linux-x86_64-centos6/R-3.3.2/lib64/R/bin/Rscript ${WGS_COPYNUM_EST_HOME}/scripts/genomescope/genomescope.R $f $k $readlen ../out/k$k
+  mkdir -p ../out/k$k
+  ${WGS_COPYNUM_EST_HOME}/scripts/genomescope/genomescope.R -i kmer-freq_k${k}.hist -o ../out/k$k -k $k
 done
 
 set +x
